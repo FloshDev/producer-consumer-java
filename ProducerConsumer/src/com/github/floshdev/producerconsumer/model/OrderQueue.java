92b0eb4e46e2dc17f01d5fb1eb59ea16b8f6e020
FloshDev <ciani.flavio04@gmail.com>
2026-03-30 11:50:34 +0200

    Add OrderQueue class

--- ProducerConsumer/src/com/github/floshdev/producerconsumer/model/OrderQueue.java
+++ ProducerConsumer/src/com/github/floshdev/producerconsumer/model/OrderQueue.java
@@ -1,8 +1,24 @@
 package com.github.floshdev.producerconsumer.model;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class OrderQueue {
-
 	
+	private LinkedList<Item> queue;
+
+	public OrderQueue() {
+		this.queue = new LinkedList<Item>();
+	}
+	
+	public void enqueue(Item item) {
+		queue.addLast(item);
+	}
+	
+	public Item dequeue() {
+		return queue.removeFirst();
+	}
+	
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
 	
 }
